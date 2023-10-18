23fa2f2d87240e39e4fff1008208ff458929a8c3
hyehyehyehyehyehye <142138962+hyehyehyehyehyehye@users.noreply.github.com>
2023-10-19 02:58:05 +0900

    모바일

--- js/layout.js
+++ js/layout.js
@@ -1,4 +1,8 @@
 $(() => {
+  const win = $(window);
+  const html = $("html");
+  const banner = $(".banner");
+
   //top으로 올라가기
   function topBtnToggle() {
     if ($(window).scrollTop() > $(window).height() / 2) {
@@ -24,5 +28,58 @@ $(() => {
   $(".btn_close").on("click", function (e) {
     e.preventDefault();
     $(".popup").hide();
+  });
+
+  //fade
+  var bannerItems = $(".banner_item");
+  var bannerSlider = $(".banner_slider");
+  var currentIndex = 0;
+
+  function showSlide(index) {
+    bannerItems.removeClass("active");
+    bannerItems.eq(index).addClass("active");
+  }
+
+  function nextSlide() {
+    currentIndex++;
+
+    if (currentIndex >= bannerItems.length) {
+      currentIndex = 0;
+
+      bannerSlider.css("transition", "none");
+      showSlide(currentIndex);
+      setTimeout(function () {
+        bannerSlider.css("transition", "100%");
+        currentIndex++;
+      }, 10);
+    } else {
+      showSlide(currentIndex);
+    }
+  }
+
+  function prevSlide() {
+    currentIndex--;
+
+    if (currentIndex < -1) {
+      currentIndex = bannerItems.length - 2;
+      bannerSlider.css("transition", "none");
+      showSlide(currentIndex);
+      setTimeout(function () {
+        bannerSlider.css("transition", "");
+        currentIndex--;
+      }, 10);
+    } else {
+      showSlide(currentIndex);
+    }
+  }
+
+  showSlide(currentIndex);
+
+  $(".prev").click(function () {
+    prevSlide();
+  });
+
+  $(".next").click(function () {
+    nextSlide();
   });
 });
